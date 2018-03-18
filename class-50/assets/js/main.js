a49690aeb00fc4f938f21666b9b45db181ab9269
didar3ar <didar3ar@gmail.com>
2018-03-18 11:04:44 +0600

    class-50

---
+++ class-50/assets/js/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+       $(".listing-carousel").owlCarousel({
+       	items: 3,
+       	loop: true,
+       	dots:true,
+       	nav:false,
+       	autoplay: true,
+       	margin:30,
+        smartSpeed: 600
+       })
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
